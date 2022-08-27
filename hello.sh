7a4ee647d360762653c7943a8d91b50a817069e5
James Alhassan <jamesalhassan052@gmail.com>
2022-08-27 21:19:10 +0000

    Read inputs and Silent inputs

--- hello.sh
+++ hello.sh
@@ -14,9 +14,18 @@ age=20
 
 echo "He is $name and his age is $age"
 
+#read User Inputs
 echo "Enter Names: "
-read name1 name2 name3
+#read name1 name2 name3
 
 echo "Names: $name1 $name2 $name3"
+
+# silent inputs
+read -p 'username: ' user_var
+read -sp 'password: ' pass_var
+echo
+echo "username : $user_var"
+echo "password : $pass_var"
+
 
 
