fe40237fdf1752517b359c894645deeab40ad489
James Alhassan <jamesalhassan052@gmail.com>
2022-08-27 22:29:25 +0000

    passin arguments

--- passArguments.sh
+++ passArguments.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo $1 $2 $3 ' > echo $1 $2 $3'
+#echo $1 $2 $3 ' > echo $1 $2 $3'
 
+#args=($@) #$@ converts arguments to array
+
+#echo ${args[0]} ${args[1]} ${args[2]}
+
+echo $@ # print all arguments
+
+echo $# # print number of argument
