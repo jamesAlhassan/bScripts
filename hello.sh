3bef06fa89009699cc647393ac96b53035bd7935
James Alhassan <jamesalhassan052@gmail.com>
2022-08-27 21:43:02 +0000

    Extracting from inputs array

--- hello.sh
+++ hello.sh
@@ -15,17 +15,21 @@ age=20
 echo "He is $name and his age is $age"
 
 #read User Inputs
-echo "Enter Names: "
+#echo "Enter Names: "
 #read name1 name2 name3
 
-echo "Names: $name1 $name2 $name3"
+#echo "Names: $name1 $name2 $name3"
 
-# silent inputs
-read -p 'username: ' user_var
-read -sp 'password: ' pass_var
+# silent inputs with "-s flag and -p for same line"
+#read -p 'username: ' user_var
+#read -sp 'password: ' pass_var
 echo
 echo "username : $user_var"
 echo "password : $pass_var"
 
+#Extracting From an Array with "-a flag"
 
+echo "Enter Names: "
+read -a names
+echo "Names: ${names[0]} , ${names[1]}"
 
