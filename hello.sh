7fca9381f6e5a51f363c28908eaee88b80639ddf
James Alhassan <jamesalhassan052@gmail.com>
2022-08-27 21:00:27 +0000

    system and user defined variables

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+echo 'Hello World'
+
+# System Defined Variables
+echo $BASH
+
+echo $BASH_VERSION
+
+echo $HOME
+
+# User Defined Variables
+name=naail
+age=20
+
+echo "He is $name and his age is $age"
