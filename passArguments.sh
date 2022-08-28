0b74542d35de3739cdc47925270ab3e63e2d93ab
James Alhassan <jamesalhassan052@gmail.com>
2022-08-28 16:44:05 +0000

    passing arguments to scripts

--- passArguments.sh
+++ passArguments.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#echo $1 $2 $3 ' > echo $1 $2 $3'
+echo "The first fruit is $1"
+echo "The second fruit is $2"
+echo "The third fruit is $3"
+echo "All fruits are $@"
+echo "The number of arguments: $#"
+
+#echo "The args are  $1 , $2 and $3"
 
 #args=($@) #$@ converts arguments to array
 
 #echo ${args[0]} ${args[1]} ${args[2]}
 
-echo $@ # print all arguments
+#echo $@ # print all arguments
 
-echo $# # print number of argument
+#echo $# # print number of argument
