459613de8f106aa9faa4c7f57dcfc19a9dc4bd80
James Alhassan <jamesalhassan052@gmail.com>
2022-08-28 21:02:30 +0000

    test operators

--- fileTestOperators.sh
+++ fileTestOperators.sh
@@ -2,14 +2,17 @@
 
 #-e = file exist
 # -f = regular file
-# -d dir
+# -d= dir
+# -b = block special files
+# -c = character special files
+# -s checks if files is empty oor not
 
 echo -e "Enter the name of the file: \c"
 read file_name
 
-if [ -e $file_name ]
+if [ -s $file_name ]
 then
-    echo "$file_name found"
+    echo "$file_name is not empty"
 else 
-    echo "$file_name not found"
+    echo "$file_name is empty"
 fi
