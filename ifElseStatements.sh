57ceb29f4750d77ee769182dd447f56c023bb3a8
James Alhassan <jamesalhassan052@gmail.com>
2022-08-28 19:27:19 +0000

    if statements

---
+++ ifElseStatements.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+value=10
+
+if [ $value < 15 ]
+then
+    echo "$value is less than 15"
+elif [ $value > 15 ]
+
+then
+
+    echo "$value is greater is greater than 15"
+else 
+    echo "$value "
+fi
